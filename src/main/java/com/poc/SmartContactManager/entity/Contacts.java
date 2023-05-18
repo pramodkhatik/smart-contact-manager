b3673bbc998e9a50dcc3b8c993f922fe9dc9f12d
uday <gurramuday70@gmail.com>
2023-05-18 14:21:15 +0530

    Added entity class for Contacts

---
+++ src/main/java/com/poc/SmartContactManager/entity/Contacts.java
@@ -0,0 +1,92 @@
+package com.poc.SmartContactManager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CONTACTS")
+public class Contacts {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cId;
+	private String firstName;
+	private String secondName;
+	private String work;
+	private String phone;
+	private String image;
+	@Column(length = 500)
+	private String description;
+	
+	public Contacts(int cId, String firstName, String secondName, String work, String phone, String image, String description) {
+		this.cId = cId;
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.work = work;
+		this.phone = phone;
+		this.image = image;
+		this.description = description;
+	}
+	
+	public int getcId() {
+		return cId;
+	}
+
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+
+}
