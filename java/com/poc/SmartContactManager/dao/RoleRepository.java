fdbeaa50287c8b2d5a4852237accfccabe34e876
Mukul Borkar <52741927+mukulborkar@users.noreply.github.com>
2023-05-24 16:09:06 +0530

    Delete java/com/poc/SmartContactManager directory

--- java/com/poc/SmartContactManager/dao/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poc.SmartContactManager.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poc.SmartContactManager.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>{
-
-}
